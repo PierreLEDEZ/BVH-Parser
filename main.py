aa6261f404449fe177393c815ec40a3eddccb0e9
LE DEZ Pierre <74079020+PierreLEDEZ@users.noreply.github.com>
2021-04-06 13:59:30 +0200

    Add example for "delete_joint" method

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from src import bvh_parser
 bvhParser = bvh_parser.BVHParser()
 
 # Parse the given file
-bvhParser.parse("./examples/scene.bvh")
+bvhParser.parse("./examples/scene_2.bvh")
+
+# Delete children of RightHand
+# By default 2nd option "inclusive" is False so in this case, kept joint named RightHand
+bvhParser.delete_joint("RightHand")
 
 # Obtain informations about bvh file
 bvhParser.get_informations()
