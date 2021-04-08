dd81452e702726ccf03f7acd3fa3a8582469ddda
LE DEZ Pierre <74079020+PierreLEDEZ@users.noreply.github.com>
2021-04-08 14:41:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ bvhParser = bvh_parser.BVHParser()
 # Parse the given file
 bvhParser.parse("./examples/scene_2.bvh")
 
-# Delete children of RightHand
-# By default 2nd option "inclusive" is False so in this case, kept joint named RightHand
+# Delete children of RightHand and LeftHand
+# By default 2nd option "inclusive" is False so in this case, kept joints named RightHand and LeftHand
 bvhParser.delete_joint("RightHand")
+bvhParser.delete_joint("LeftHand")
 
 # Obtain informations about bvh file
 bvhParser.get_informations()
